e0fe57a2ec0223bd82e4bafab186ee2b2e848543
shiftreg <marcojehle@gmx.net>
2021-05-01 00:46:50 +0200

    Feat: first red incredible circle

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,15 +1,17 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import Circle from "react-colorful-circle";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View } from "../components/Themed";
 
 export default function TabTwoScreen() {
+  // const [currentColor, setstate] = useState(initialState)
+  const Circle = () => {
+    return <View style={styles.circle} />;
+  };
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Becher App Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+    <View>
+      <Circle></Circle>
     </View>
   );
 }
@@ -17,16 +19,22 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  circle: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    backgroundColor: "red",
   },
 });
