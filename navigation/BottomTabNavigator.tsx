7e613c02463abb83c91fcba48c1fd6771c40a827
shiftreg <marcojehle@gmx.net>
2021-04-30 23:45:51 +0200

    get high

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -57,7 +57,7 @@ function TabOneNavigator() {
       <TabOneStack.Screen
         name="TabOneScreen"
         component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
+        options={{ headerTitle: 'Get High O_o!' }}
       />
     </TabOneStack.Navigator>
   );
